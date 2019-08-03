93ce0412b7b032b4c68e169f39c4cd342939082c
Zhong GuiYong <814374557@qq.com>
2019-08-03 12:11:15 +0800

    调整文章的model结构。增加md存档以及解析后的文字存档，评论，文章类型，描述。创建时间和更新时间等。

--- routes/v1/article/index.js
+++ routes/v1/article/index.js
@@ -57,13 +57,22 @@ article.post('/', checkAuth, function (req, res, next) {
   let title = req.body.title ? req.body.title : ''
   let content = req.body.content ? req.body.content : ''
   let fans = req.body.fans ? JSON.parse(req.body.fans) : []
-
+  let desc = req.body.desc ? req.body.desc : ''
+  let mdContent = req.body.mdContent ? req.body.mdContent : ''
+  let parseContent = req.body.parseContent ? req.body.parseContent : ''
+  let tags = req.body.tags ? req.body.tags : []
+  let type = req.body.type ? req.body.type : ''
   const article = new Article({
     _id: new mongoose.Types.ObjectId(),
     author: author,
     title: title,
     content: content,
-    fans: fans
+    fans: fans,
+    desc: desc,
+    mdContent: mdContent,
+    parseContent: parseContent,
+    type: type,
+    tags: tags
   })
   console.log(article)
   // res.end(200)
@@ -80,5 +89,33 @@ article.post('/', checkAuth, function (req, res, next) {
     })
   })
 })
+article.put('/:id', function (req, res, next) {
+  const id = req.params.id
+  const updateObj = {
+    title: req.body.title ? req.body.title : '',
+    content: req.body.content ? req.body.content : '',
+    fans: req.body.fans ? JSON.parse(req.body.fans) : [],
+    desc: req.body.desc ? req.body.desc : '',
+    mdContent: req.body.mdContent ? req.body.mdContent : '',
+    parseContent: req.body.parseContent ? req.body.parseContent : '',
+    tags: req.body.tags ? req.body.tags : [],
+    type: req.body.type ? req.body.type : ''
+  }
 
+  for (var i in updateObj) {
+    if (updateObj[i] === '' || updateObj[i].length === 0) {
+      delete updateObj[i]
+    }
+  }
+  console.log(updateObj)
+  Article.update({ _id: id }, updateObj)
+    .exec()
+    .then(result => { console.log(result); res.status(200).json({ result: result }) })
+    .catch(err => {
+      // console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+})
 export default article
