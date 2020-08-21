b572075e0647b3c3fd9a0f2b4a5eb934234e8d54
ZhongGuiYong <zhongguiyong@chicv.com>
2020-08-21 10:18:22 +0800

    修改标题.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '仿模板编辑 - by Tom Zhong' });
 });
 
 module.exports = router;
